5495c5c3034e4d545cd774e4bde45bedb03b119b
Abhijit Kulkarni <akulkarni@meetveracity.com>
2019-09-23 11:22:25 -0400

    Removed all the DB related files from master

--- bip-archetype-service-origin/bip-origin/src/main/java/gov/va/bip/origin/impl/OriginServiceImpl.java
+++ bip-archetype-service-origin/bip-origin/src/main/java/gov/va/bip/origin/impl/OriginServiceImpl.java
@@ -23,13 +23,11 @@ import gov.va.bip.framework.log.BipLogger;
 import gov.va.bip.framework.log.BipLoggerFactory;
 import gov.va.bip.framework.messages.MessageKeys;
 import gov.va.bip.framework.messages.MessageSeverity;
-import gov.va.bip.framework.validation.Defense;
 import gov.va.bip.origin.OriginService;
-import gov.va.bip.origin.data.SampleDataHelper;
-import gov.va.bip.origin.data.sampledatasource2.entities.SampleData2;
 import gov.va.bip.origin.messages.OriginMessageKeys;
 import gov.va.bip.origin.model.SampleDomainRequest;
 import gov.va.bip.origin.model.SampleDomainResponse;
+import gov.va.bip.origin.model.SampleInfoDomain;
 import gov.va.bip.origin.utils.CacheConstants;
 import gov.va.bip.origin.utils.HystrixCommandConstants;
 
@@ -52,11 +50,6 @@ public class OriginServiceImpl implements OriginService {
 	/** Bean name constant */
 	public static final String BEAN_NAME = "originServiceImpl";
 
-	/** The origin web service database operations helper. */
-	@Autowired
-	private SampleDataHelper sampleDataHelper;
-
-	/** The cache manager */
 	@Autowired
 	private CacheManager cacheManager;
 
@@ -65,8 +58,6 @@ public class OriginServiceImpl implements OriginService {
 	 */
 	@PostConstruct
 	void postConstruct() {
-		// Check for WS Client ref. Note that cacheManager is allowed to be null.
-		Defense.notNull(sampleDataHelper, "Unable to proceed with database request. The sampleDataHelper must not be null.");
 	}
 
 	/**
@@ -81,8 +72,8 @@ public class OriginServiceImpl implements OriginService {
 	 */
 	@Override
 	@CachePut(value = CacheConstants.CACHENAME_ORIGIN_SERVICE,
-	key = "#root.methodName + T(gov.va.bip.framework.cache.BipCacheUtil).createKey(#sampleDomainRequest.participantID)",
-	unless = "T(gov.va.bip.framework.cache.BipCacheUtil).checkResultConditions(#result)")
+			key = "#root.methodName + T(gov.va.bip.framework.cache.BipCacheUtil).createKey(#sampleDomainRequest.participantID)",
+			unless = "T(gov.va.bip.framework.cache.BipCacheUtil).checkResultConditions(#result)")
 	/* If a fallback position is possible, add attribute to @HystrixCommand: fallback="fallbackMethodName" */
 	@HystrixCommand(commandKey = "SampleFindByParticipantIDCommand",
 			ignoreExceptions = { IllegalArgumentException.class, BipException.class, BipRuntimeException.class })
@@ -106,18 +97,13 @@ public class OriginServiceImpl implements OriginService {
 
 		LOGGER.debug("sampleFindByParticipantID no cached data found");
 
-		// try from database helper
-		SampleData2 data = null;
-		data = sampleDataHelper.getSampleDataForPid(sampleDomainRequest.getParticipantID());
-		if (data == null) {
-			response.addMessage(MessageSeverity.INFO, HttpStatus.OK,
-					OriginMessageKeys.BIP_SAMPLE_SERVICE_DATABASE_CALL_RETURNED_NULL, "");
-		} else {
-			response.addMessage(MessageSeverity.INFO, HttpStatus.OK,
-					OriginMessageKeys.BIP_SAMPLE_SERVICE_DATABASE_CALL_PERFORMED, "");
-		}
-
-		// return results of database call
+		// send hard coded data ... normally would get from db or partner
+		SampleInfoDomain sampleInfoDomain = new SampleInfoDomain();
+		sampleInfoDomain.setName("JANE DOE");
+		sampleInfoDomain.setParticipantId(sampleDomainRequest.getParticipantID());
+		response.setSampleInfo(sampleInfoDomain);
+		response.addMessage(MessageSeverity.INFO, HttpStatus.OK, OriginMessageKeys.BIP_SAMPLE_SERVICE_IMPL_RESPONDED_WITH_MOCK_DATA,
+				"");
 		return response;
 	}
 
