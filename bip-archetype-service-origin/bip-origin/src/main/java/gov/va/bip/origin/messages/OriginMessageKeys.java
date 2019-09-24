3ca4100e84bec7c348da33148d2ba43ee6f26c9d
Abhijit Kulkarni <akulkarni@meetveracity.com>
2019-09-24 13:15:28 -0400

    merge from master

--- bip-archetype-service-origin/bip-origin/src/main/java/gov/va/bip/origin/messages/OriginMessageKeys.java
+++ bip-archetype-service-origin/bip-origin/src/main/java/gov/va/bip/origin/messages/OriginMessageKeys.java
@@ -16,18 +16,28 @@ public enum OriginMessageKeys implements MessageKey {
 	/** Minimum allowed value validation for PID; no args */
 	BIP_SAMPLE_REQUEST_PID_MIN("Min.sampleRequest.participantID",
 			"SampleRequest.participantID cannot be zero"),
+	/** Minimum allowed value validation for PID; no args */
+	BIP_SAMPLE_DATA_REQUEST_PID_MIN("Min.sampleDataRequest.participantID", "SampleDataRequest.participantID cannot be zero"),
 	/** PID cannot be null validation; no args */
 	BIP_SAMPLE_REQUEST_PID_NOTNULL("NotNull.sampleRequest.participantID",
 			"SampleRequest.participantID cannot be null"),
 	/** Payload cannot be null validation; no args */
 	BIP_SAMPLE_REQUEST_NOTNULL("NotNull.sampleRequest",
 			"SampleRequest Payload cannot be null"),
+	/** Payload cannot be null validation; no args */
+	BIP_SAMPLE_DATA_NOTNULL("NotNull.sampleData1", "SampleData1 Payload cannot be null"),
 	/** Response has different PID than the request; no args */
 	BIP_SAMPLE_REQUEST_PID_INCONSISTENT("bip.origin.sample.request.pid.inconsistent",
 			"Response returned an invalid Participant ID."),
 	/** Response has different PID than the logged in user; no args */
 	BIP_SAMPLE_REQUEST_PID_INVALID("bip.origin.sample.request.pid.invalid",
 			"Response has different PID than the logged in user."),
+	/** Pid value could not be found; no args */
+	BIP_SAMPLE_DATA_REQUEST_PID_NOT_FOUND("bip.origin.pid.not.found",
+			"Pid value could not be found."),
+	BIP_SAMPLE_SERVICE_DATABASE_CALL_PERFORMED("bip.origin.sample.service.impl.database.call.performed", "Call to database was made."),
+	BIP_SAMPLE_SERVICE_DATABASE_CALL_RETURNED_NULL("bip.origin.sample.service.impl.database.call.returned.null",
+			"Call to database returned null."),
 	BIP_SAMPLE_SERVICE_IMPL_RESPONDED_WITH_PARTNER_MOCK_DATA("bip.origin.sample.service.impl.responded.with.partner.mock.data",
 			"Response sent from service impl with partner mock data."),
 	BIP_SAMPLE_SERVICE_IMPL_RESPONDED_WITH_MOCK_DATA("bip.origin.sample.service.impl.responded.with.mock.data",
